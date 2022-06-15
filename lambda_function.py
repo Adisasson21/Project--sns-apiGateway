e86ca9bc2636b205738774ed84f67f9ca156f83a
Adisasson21 <96379294+Adisasson21@users.noreply.github.com>
2022-06-15 17:09:06 +0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,8 @@ import boto3
 def lambda_handler(event, context):
     list1 = event.values()
     sum_calc = sum(list1)
+    
+    ---  #Added this content to the function for displaying a message in an email 
     notification = f'Here is the SNS notification for Lambda function - the sum calculate is {(lambda x: x)(sum_calc)}'
     client = boto3.client('sns')
     response = client.publish (
@@ -11,7 +13,7 @@ def lambda_handler(event, context):
       Message = json.dumps({'default': notification}),
       MessageStructure = 'json'
     )
-
+    ---
     return {
       'statusCode': 200,
       'body': json.dumps(response),
